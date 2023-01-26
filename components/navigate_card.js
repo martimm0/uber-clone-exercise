623947a8c6b365a3c3dcec77008c4ef4e7132a2d
Miles <107070051+martimm0@users.noreply.github.com>
2023-01-26 16:24:36 -0500

    Create navigate_card.js

---
+++ components/navigate_card.js
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import tw from "tailwind-react-native-classnames";
+import { API_KEY } from "@env";
+import { setDestination } from "../slices/navSlice";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import NavFavorites from "./NavFavorites";
+import { Icon } from "react-native-elements";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Hello!</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyles}
+            placeholder="Where to?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            minLength={2}
+            enablePoweredByContainer={false}
+            returnKeyType={"search"}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+
+              navigation.navigate("RideOptionsCard");
+            }}
+            fetchDetails={true}
+            query={{
+              key: API_KEY,
+              language: "en",
+            }}
+          />
+        </View>
+        <NavFavorites />
+      </View>
+
+      <View style={tw`flex-row bg-white justify-evenly`}>
+        <TouchableOpacity
+          style={tw`flex flex-row bg-black w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row bg-white w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw`text-black text-center`}>Eats</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
