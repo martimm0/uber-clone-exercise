61bb0dd9bf10af632e54bf38d22886c47ff65530
Miles <107070051+martimm0@users.noreply.github.com>
2023-01-26 16:31:23 -0500

    Create home_screen.js

---
+++ screens/home_screen.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavorites from "../components/NavFavorites";
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          placeholder="Where from?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          query={{
+            key: API_KEY,
+            language: "en",
+          }}
+        />
+        <NavOptions />
+        <NavFavorites />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
